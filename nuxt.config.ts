0c78a190706e0023e3faeb13db98ade5c817f225
david <davidostos2@gmail.com>
2024-05-06 13:18:38 -0400

    subienod meta para la exposicionde de datos de red social

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,13 @@ export default defineNuxtConfig({
   ssr: true,
   app: {
     head:{
+      meta:[
+        {property: "og:title", content: "David Ostos - portafolio"},
+        {property: "og:description", content: "Portafolio de mis habilidades y experiencias"},
+        {property: "og:image", content: "https://swansoftwaresolutions.com/wp-content/uploads/2020/04/05.14.20-Meet-a-Full-Stack-Developer-Vlad-Ryba.jpg"},
+        {property: "og:url", content: "https://davidostos.netlify.app/"}
+
+      ],
       link: [
         { rel: "preconnect" , href: "https://fonts.gstatic.com" },
         { rel: "stylesheet" , href: "https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+New&display=swap"}
